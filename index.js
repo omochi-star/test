bd1b17e6b7816bdab32e82c6f735729ea34f83aa
omochi-star <omochim275@gmail.com>
2025-06-30 22:09:45 +0900

    ejs-mateを使用

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const methodOverride = require('method-override')
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const engine = require('ejs-mate');
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/bookreview';
 
@@ -19,13 +20,13 @@ mongoose.connect(dbUrl,
         console.log(err);
     });
 
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
-app.set('views', path.join(__dirname, 'views'));
-
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('home');
