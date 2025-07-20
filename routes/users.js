a81229a31b5a6bc599f5d59a86780912ab08c7f0
omochi-star <omochim275@gmail.com>
2025-07-20 20:09:55 +0900

    ログイン実装7

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
@@ -10,14 +11,24 @@ router.post('/register', async (req, res) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
-        const registerdUser = await User.register(user, password);
-        console.log(registerdUser);
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
         req.flash('success', 'ユーザーを新規登録しました');
         res.redirect('/bookreview');
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
     }
+});
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    const { username } = req.body;
+    req.flash('success', `${username}さん、おかえりなさい！`);
+    res.redirect('/bookreview');
 });
 
 module.exports = router;
