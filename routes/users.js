77e71708fa14c5aeb8e2b0f87431b31b6c11257b
omochi-star <omochim275@gmail.com>
2025-07-21 20:43:29 +0900

    req.login追加してユーザ登録時にログインする

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,16 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.flash('success', 'ユーザーを新規登録しました');
-        res.redirect('/bookreview');
+        req.login(registeredUser, err => {
+            if (err) return next(err);
+            req.flash('success', 'ユーザーを新規登録しました');
+            res.redirect('/bookreview');
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('/register');
