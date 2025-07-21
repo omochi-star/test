001d0e4d0b4550a2026a18972bee1528eb306d7c
omochi-star <omochim275@gmail.com>
2025-07-21 20:33:50 +0900

    req.userをどのテンプレートからも使えるようにする

--- routes/users.js
+++ routes/users.js
@@ -31,4 +31,12 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     res.redirect('/bookreview');
 });
 
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        req.flash('success', 'ログアウトしました')
+        res.redirect('/bookreview');
+    });
+});
+
 module.exports = router;
