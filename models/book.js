ef87c7dc008859d2ab59ad5f88707f332ed71dd2
omochi-star <omochim275@gmail.com>
2025-07-31 20:58:12 +0900

    book消したらReviewも削除

--- models/book.js
+++ models/book.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const Review = require('./review');
 
 const bookSchema = new mongoose.Schema({
     title: String,
@@ -11,6 +12,12 @@ const bookSchema = new mongoose.Schema({
     owner: {
         type: Schema.Types.ObjectId,
         ref: 'User'
+    }
+});
+
+bookSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({ book: doc._id });
     }
 });
 
