6908a9927720cfa5eac3edf987ed42903818883b
omochi-star <omochim275@gmail.com>
2025-08-07 21:19:54 +0900

    サムネイル用のバーチャルプロパティ

--- models/book.js
+++ models/book.js
@@ -2,21 +2,28 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const Review = require('./review');
 
+const imageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+imageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200')
+});
+
 const bookSchema = new mongoose.Schema({
     title: String,
     author: String,
     isbn: String,
     category: String,
-    images: [{
-        url: String,
-        filename: String
-    }],
+    images: [imageSchema],
     description: String,
     owner: {
         type: Schema.Types.ObjectId,
         ref: 'User'
     }
 });
+
 
 bookSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
